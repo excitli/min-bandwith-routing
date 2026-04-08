81cf7266b34850940bf8a1e8d8cf737462cc564f
excitli <>
2026-04-08 20:17:30 +0300

    added "optimize" endpoint; for now it only looks for K shortest paths

--- app/api/routes/optimization.py
+++ app/api/routes/optimization.py
@@ -1,8 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.dependencies import get_db
+from app.schemas.optimize import OptimizeRequest
+from app.optimization.shortest_paths import generate_k_shortest_paths
+from app.api.routes.topology import get_graph_by_scenario
+from app.api.routes.demands import get_demands_by_scenario
+from app.additional.build_graph import build_graph
+import networkx as nx
+
+
+
 
 router = APIRouter()
 
 
 @router.post("/")
-async def run_optimization():
+async def optimize(
+        data: OptimizeRequest,
+        db: AsyncSession = Depends(get_db)
+):
+    topology_data = await get_graph_by_scenario(data.scenario_id, db)
+    graph = topology_data["graph"]
+    demands = await get_demands_by_scenario(data.scenario_id, db)
+
+    paths = generate_k_shortest_paths(graph, k_paths = data.k_paths, demands=demands)
+    return {
+        "status": "ok",
+        "paths": paths,
+    }
+
     return {"status": "ok"}
