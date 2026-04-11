af19f59473c388f53ca5c415fc60b005b05888c7
excitli <>
2026-04-11 17:28:11 +0300

    changes: now returns results from solver added: try except block (since a user may send such demands that is won't be possible to optimize it)

--- app/api/routes/optimization.py
+++ app/api/routes/optimization.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.dependencies import get_db
+from app.optimization.solver import solve_routing
 from app.schemas.optimize import OptimizeRequest
 from app.optimization.shortest_paths import generate_k_shortest_paths
 from app.api.routes.topology import get_graph_by_scenario
@@ -24,9 +25,19 @@ async def optimize(
     demands = await get_demands_by_scenario(data.scenario_id, db)
 
     paths = generate_k_shortest_paths(graph, k_paths = data.k_paths, demands=demands)
+    try:
+        results = solve_routing(
+            edges=topology_data["edges"],
+            demands=demands,
+            paths=paths,
+            routing_type=data.routing_type,
+            objective_type=data.optimization_objective,
+        )
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Optimization is impossible for such demands.")
     return {
-        "status": "ok",
-        "paths": paths,
+        "results": results
     }
+
 
     return {"status": "ok"}
