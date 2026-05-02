6d86542402a55565a42788ec58918b391797f75d
excitli <>
2026-05-02 20:56:58 +0300

    added a new ORM model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from backend.app.models.demand import Demand
 from backend.app.models.edge import Edge
 from backend.app.models.node import Node
 from backend.app.models.scenario import Scenario
+from backend.app.models.optimizationResults import OptimizationResults
 
-__all__ = ["Demand", "Edge", "Node", "Scenario"]
+__all__ = ["Demand", "Edge", "Node", "Scenario", "OptimizationResults"]
---
+++ backend/app/models/optimizationResults.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Float, ForeignKey, Integer, Column, String
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Any, List, Dict
+
+
+from backend.app.models.base import Base
+
+
+class OptimizationResults(Base):
+    __tablename__ = 'optimization_results'
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    scenario_id: Mapped[int] = Column(Integer, ForeignKey('scenarios.id'))
+    objective: Mapped[str] = Column(String, nullable=False)
+    routing_type: Mapped[str] = Column(String, nullable=False)
+    objective_value: Mapped[float] = Column(Float, nullable=False)
+    paths: Mapped[List[Any]] = Column(String, nullable=False)
+    duals: Mapped[List[Any]] = Column(String, nullable=False)
+
