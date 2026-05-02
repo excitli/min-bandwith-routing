14f532070b4f5b2c8fb47967ea212a4e3d6c38d4
excitli <>
2026-05-02 20:58:11 +0300

    added 2 endpoints

--- backend/app/api/routes/optimization.py
+++ backend/app/api/routes/optimization.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from backend.app.core.dependencies import get_db
 from backend.app.optimization.solver import solve_routing
-from backend.app.schemas.optimize import OptimizeRequest
+from backend.app.schemas.optimize import OptimizeRequest, OptimizeResult
 from backend.app.optimization.shortest_paths import generate_k_shortest_paths
 from backend.app.api.routes.topology import get_graph_by_scenario
 from backend.app.api.routes.demands import get_demands_by_scenario
+from backend.app.models.optimizationResults import OptimizationResults
+import json
+from typing import List
+
 
 router = APIRouter()
 
@@ -35,4 +40,54 @@ async def optimize(
     }
 
 
-    return {"status": "ok"}
+@router.post(
+    "/results",
+    status_code=status.HTTP_201_CREATED,
+    )
+async def optimize_results(
+        data: OptimizeResult,
+        db: AsyncSession = Depends(get_db),
+):
+    paths_json = json.dumps(data.paths)
+    duals_json = json.dumps(data.duals)
+    new_result = OptimizationResults(
+        scenario_id=data.scenario_id,
+        objective=data.objective,
+        routing_type=data.routing_type,
+        objective_value=data.objective_value,
+        paths=paths_json,
+        duals=duals_json,
+    )
+
+    async with db.begin():
+        db.add(new_result)
+
+    await db.refresh(new_result)
+
+    return {"status": "POSTED"}
+
+
+@router.get(
+    "/optimizeResults/{scenario_id}",
+    response_model=List[OptimizeResult]
+)
+async def optimize_results(
+        scenario_id: int,
+        db: AsyncSession = Depends(get_db)):
+    stmt = select(OptimizationResults).where(
+        OptimizationResults.scenario_id == scenario_id
+    )
+    result = await db.execute(stmt)
+    orm_objects = result.scalars().all()
+
+    return [
+        OptimizeResult(
+        scenario_id=obj.scenario_id,
+        objective=obj.objective,
+        routing_type=obj.routing_type,
+        objective_value=obj.objective_value,
+        paths=json.loads(obj.paths),
+        duals=json.loads(obj.duals)
+    )
+         for obj in orm_objects
+    ]
