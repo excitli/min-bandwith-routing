ee273ab6f8e075cee847d24bba503c600d21cc3f
excitli <>
2026-05-18 15:59:34 +0300

    added cpla

--- backend/app/optimization/shortest_paths.py
+++ backend/app/optimization/shortest_paths.py
@@ -32,3 +32,36 @@ def generate_k_shortest_paths(graph: nx.DiGraph, k_paths: int, demands: list) ->
             continue
 
     return all_paths_data
+
+#for cpla
+def generate_shortest_paths_by_weights(graph: nx.DiGraph, demands: list, edge_weights: dict) -> list:
+    new_paths = []
+
+    for u, v, data in graph.edges(data=True):
+        edge_id = data['id']
+        graph[u][v]['dynamic_weight'] = edge_weights.get(edge_id, data.get('weight', 1.0) + 1e-4)
+
+    for demand in demands:
+        try:
+            node_path = nx.shortest_path(graph, source=demand.source, target=demand.target, weight='dynamic_weight')
+            node_pairs = list(zip(node_path, node_path[1:]))
+
+            path_edge_ids = []
+            path_total_weight = 0
+
+            for u, v in node_pairs:
+                edge_data = graph[u][v]
+                path_edge_ids.append(edge_data['id'])
+                path_total_weight += edge_data.get('weight', 1.0)  # Сохраняем физический вес, а не динамический!
+
+            new_paths.append({
+                "demand_id": demand.id,
+                "nodes": node_path,
+                "edge_ids": path_edge_ids,
+                "length": len(path_edge_ids),
+                "weight": path_total_weight,
+            })
+        except (nx.NetworkXNoPath, nx.NodeNotFound):
+            continue
+
+    return new_paths
